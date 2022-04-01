feb128613d5d462225c48166e855f4d326a0a49d
ohhsodead <iashx307@gmail.com>
2022-04-01 05:13:46 +0100

    update file name

--- src/ModioX/Languages/en_US.Designer.cs
+++ src/ModioX/Languages/en_US.Designer.cs
@@ -22,14 +22,14 @@ namespace ModioX.Languages {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class en {
+    internal class en_US {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal en() {
+        internal en_US() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace ModioX.Languages {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModioX.Languages.en", typeof(en).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ModioX.Languages.en_US", typeof(en_US).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
